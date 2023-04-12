7c17ce91eee95509f4bf107e3bb9eeab029c0690
Tonyeli Tay <taytonyeli@gmail.com>
2023-04-12 02:39:21 +0000

    feat: setup basic landing page

---
+++ resources/views/validate.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Subscriber Manager | Home</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body class="antialiased">
+        <main class=" container mx-auto p-4">
+        <h1 class="text-center text-2xl">Welcome to MailerLite Subscriber Manager</h1>
+        
+        </main>
+    </body>
+</html>
