ad761e156be32d4b3b13159fe18d6bddc8f3e1cd
Tonyeli Tay <taytonyeli@gmail.com>
2023-04-15 18:46:26 +0000

    tests: test creating a subscriber

--- tests/Feature/ManageSubscribersTest.php
+++ tests/Feature/ManageSubscribersTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature;
 
 use App\Helpers\MailerLiteClient;
 use App\Models\Account;
+use App\Models\MailerLiteSubscriber;
+use Faker\Factory;
 use Tests\TestCase;
 
 class ManageSubscribersTest extends TestCase
@@ -39,6 +41,55 @@ class ManageSubscribersTest extends TestCase
 
         $account->delete();
         $this->assertDeleted($account);
+    }
+
+    /**
+     * Test fetching subscriber data via API.
+     *
+     * @return void
+     */
+    public function test_creating_subscriber()
+    {
+        $faker = Factory::create();
+        $mailerLiteCLient = new MailerLiteClient(env('MAILER_LITE_API_KEY', '1111'));
+        $subscriber = new MailerLiteSubscriber(
+            $faker->email(),
+            $faker->name(),
+            $faker->country()
+        );
+        $result = $mailerLiteCLient->createSubscriber($subscriber);
+        // dd($result);
+        $this->assertIsArray($result);
+        $this->assertArrayHasKey("message", $result);
+        $this->assertArrayNotHasKey("errors", $result);
+        $this->assertStringContainsString("Created", $result["message"]);
+
+    }
+
+    /**
+     * Test fetching subscriber data via API.
+     *
+     * @return void
+     */
+    public function test_creating_subscriber_via_api()
+    {
+        $account = Account::factory()->create([
+            'api_key' => env('MAILER_LITE_API_KEY', '1111'),
+        ]);
+
+        $faker = Factory::create();
+        $subscriberResponse = $this->post('/create-subscriber',
+            [
+                "email" => $faker->email(),
+                "name" => $faker->name(),
+                "country" => $faker->country(),
+            ]);
+        $subscriberResponse->assertStatus(200);
+
+        
+        $account->delete();
+        $this->assertDeleted($account);
+
     }
 
     public function tearDown(): void
