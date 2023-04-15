d538a7599a6655e5fb082f0b41944c983ea841e1
Tonyeli Tay <taytonyeli@gmail.com>
2023-04-15 04:44:11 +0000

    tests: test datable server requirements

---
+++ tests/Feature/DataTablesTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Account;
+use Tests\TestCase;
+
+class DataTablesTest extends TestCase
+{
+    // public function setUp(): void
+    // {
+    //     Account::factory()->create([
+    //         'api_key' => env('MAILER_LITE_API_KEY', '1111'),
+    //     ]);
+    //     parent::setUp();
+    // }
+
+    /**
+     * Test that basic data tables meta data is in response.
+     *
+     * @return void
+     */
+    public function test_subscribers_has_data_tables_meta()
+    {
+        $account = Account::factory()->create([
+            'api_key' => env('MAILER_LITE_API_KEY', '1111'),
+        ]);
+        $params = http_build_query([
+            "draw" => 7,
+            "length" => 10,
+            "start" => 30,
+        ]);
+        $response = $this->get('/api/v1/subscribers?' . $params);
+
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'draw',
+            'recordsTotal',
+            'recordsFiltered',
+            'data',
+        ]);
+        $account->delete();
+
+    }
+
+    /**
+     * Ensure draw is being updated.
+     *
+     * @return void
+     */
+    public function test_subscribers_updated_draw()
+    {
+        $account = Account::factory()->create([
+            'api_key' => env('MAILER_LITE_API_KEY', '1111'),
+        ]);
+        $params = http_build_query([
+            "draw" => 7,
+            "length" => 10,
+            "start" => 30,
+        ]);
+        $response = $this->get('/api/v1/subscribers?' . $params);
+
+        $response->assertStatus(200);
+        $response->assertJsonPath('draw', 8);
+        $account->delete();
+
+    }
+
+    public function tearDown(): void
+    {
+        Account::truncate();
+        parent::tearDown();
+    }
+}
